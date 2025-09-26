75f995c9f292eabfd48e53afa6fcf1871cc57fc7
Artem Poliakov <artem4250@gmail.com>
2025-09-26 13:16:52 +0300

    update: converter.py, data_processing.ipynb, training.ipynb, metrics.json

--- lab1/python/converter.py
+++ lab1/python/converter.py
@@ -4,17 +4,17 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def wav_to_spectrogram(wav_path, img_path):
+def wav_to_spectrogram(wav_path, img_path, image_size=112):
     try:
         y, sr = librosa.load(wav_path, sr=None)
-        S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128)
+        S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=image_size)
         S_dB = librosa.power_to_db(S, ref=np.max)
 
-        plt.figure(figsize=(4, 4))
-        librosa.display.specshow(S_dB, sr=sr, cmap='magma')
-        plt.axis('off')
+        fig, ax = plt.subplots(figsize=(0.01*image_size, 0.01*image_size), dpi=100)
+        librosa.display.specshow(S_dB, sr=sr, cmap='gray', ax=ax)
+        ax.axis('off')
         plt.tight_layout(pad=0)
-        plt.savefig(img_path, bbox_inches='tight', pad_inches=0)
-        plt.close()
+        plt.savefig(img_path, dpi=100, bbox_inches='tight', pad_inches=0)
+        plt.close(fig)
     except Exception as e:
         print(f"Error processing {wav_path}: {e}")
